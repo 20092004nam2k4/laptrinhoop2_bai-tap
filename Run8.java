eabe4efb55112794e514237d0d995d8c24d88633
20092004nam2k4 <114391153+20092004nam2k4@users.noreply.github.com>
2023-04-21 11:16:55 +0700

    bai3

---
+++ Run8.java
@@ -0,0 +1,10 @@
+package baitap.bai3;
+
+public class Run8 {
+  public static void main(String[] args) {
+    MovablePoint movablePoint = new MovablePoint ();
+    System.out.println (movablePoint);
+    movablePoint = new MovablePoint (3,4,5,6);
+    System.out.println (movablePoint);
+  }
+}
